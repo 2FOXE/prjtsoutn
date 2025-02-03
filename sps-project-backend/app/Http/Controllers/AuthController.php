24a70fb64adfd7e6602ce4387b9b237bc7be3ff2
abdessamad <moroccangamer1992@gmail.com>
2025-02-03 23:44:19 +0100

    Add AuthController for user authentication and create migration for chambre table modification

---
+++ sps-project-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['These credentials do not match our records.'],
+            ]);
+        }
+
+        // Create a new token for the user
+        $token = $user->createToken('YourAppName')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}
